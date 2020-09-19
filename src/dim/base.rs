e6cb6f738da8220ecf8c9def0782416be0375827
jerry73204 <jerry73204@gmail.com>
2020-09-19 16:58:02 +0800

    Organize dim mod

--- src/dim/base.rs
+++ src/dim/base.rs
@@ -1,165 +1,182 @@
 use super::{impls, ops};
 use crate::{common::*, dims_t, DimsT};
 
-/// Marks the list of dimensions.
-pub trait Dimensions
-where
-    Self: Sized,
-{
-    fn to_dyn(&self) -> DynDimensions
-    where
-        (): impls::ToDynImpl<Self>,
-    {
-        <() as impls::ToDynImpl<Self>>::impl_to_dyn(self)
-    }
+pub use dim::*;
+pub use dimensions::*;
 
-    fn new_dyn_dims(dims: Vec<usize>) -> DynDimensions {
-        DynDimensions(dims)
-    }
+mod dimensions {
+    use super::*;
 
-    fn new_dims1<P>(first: P) -> Dims1<P::Output>
-    where
-        P: impls::IntoDim,
-    {
-        dims_t![first.into_dim()]
-    }
+    // dynamic type
 
-    fn new_dims2<P, Q>(first: P, second: Q) -> Dims2<P::Output, Q::Output>
-    where
-        P: impls::IntoDim,
-        Q: impls::IntoDim,
-    {
-        dims_t![first.into_dim(), second.into_dim()]
-    }
+    /// The dimensions with runtime length.
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct DynDimensions(pub Vec<usize>);
 
-    fn new_dims3<P, Q, R>(first: P, second: Q, third: R) -> Dims3<P::Output, Q::Output, R::Output>
-    where
-        P: impls::IntoDim,
-        Q: impls::IntoDim,
-        R: impls::IntoDim,
-    {
-        dims_t![first.into_dim(), second.into_dim(), third.into_dim()]
-    }
+    // aliases
 
-    fn len(&self) -> usize
-    where
-        (): impls::LenImpl<Self>,
-    {
-        <() as impls::LenImpl<Self>>::impl_len(self)
-    }
+    pub type Dims1<P> = DimsT![P];
+    pub type Dims2<P, Q> = DimsT![P, Q];
+    pub type Dims3<P, Q, R> = DimsT![P, Q, R];
 
-    fn to_vec(&self) -> Vec<usize>
-    where
-        (): impls::ToVecImpl<Self>,
-    {
-        <() as impls::ToVecImpl<_>>::impl_to_vec(self)
-    }
-
-    fn matrix_dot<Rhs>(
-        &self,
-        rhs: &Rhs,
-    ) -> impls::MatrixDotImplOp<Self, Rhs, ops::MatrixDotOp<Self, Rhs>>
+    /// Marks the list of dimensions.
+    pub trait Dimensions
     where
         Self: Sized,
-        Rhs: Dimensions,
-        (): ops::MatrixDot<Self, Rhs>
-            + impls::MatrixDotImpl<Self, Rhs, ops::MatrixDotOp<Self, Rhs>>,
     {
-        <() as impls::MatrixDotImpl<Self, Rhs, ops::MatrixDotOp<Self, Rhs>>>::impl_matrix_dot(
-            self, rhs,
-        )
-    }
-}
+        fn to_dyn(&self) -> DynDimensions
+        where
+            (): impls::ToDynImpl<Self>,
+        {
+            <() as impls::ToDynImpl<Self>>::impl_to_dyn(self)
+        }
 
-impl Dimensions for DynDimensions {}
+        // fn new_dyn_dims(dims: Vec<usize>) -> DynDimensions {
+        //     DynDimensions(dims)
+        // }
 
-impl<Head, Tail> Dimensions for Cons<Head, Tail>
-where
-    Head: Dim,
-    Tail: DimsList,
-{
-}
+        // fn new_dims1<P>(first: P) -> Dims1<P::Output>
+        // where
+        //     P: impls::IntoDim,
+        // {
+        //     dims_t![first.into_dim()]
+        // }
 
-impl Dimensions for Nil {}
+        // fn new_dims2<P, Q>(first: P, second: Q) -> Dims2<P::Output, Q::Output>
+        // where
+        //     P: impls::IntoDim,
+        //     Q: impls::IntoDim,
+        // {
+        //     dims_t![first.into_dim(), second.into_dim()]
+        // }
 
-pub trait DimsList
-where
-    Self: List + Dimensions,
-{
-}
+        // fn new_dims3<P, Q, R>(first: P, second: Q, third: R) -> Dims3<P::Output, Q::Output, R::Output>
+        // where
+        //     P: impls::IntoDim,
+        //     Q: impls::IntoDim,
+        //     R: impls::IntoDim,
+        // {
+        //     dims_t![first.into_dim(), second.into_dim(), third.into_dim()]
+        // }
 
-impl<Head, Tail> DimsList for Cons<Head, Tail>
-where
-    Head: Dim,
-    Tail: DimsList,
-{
-}
+        fn len(&self) -> usize
+        where
+            (): impls::LenImpl<Self>,
+        {
+            <() as impls::LenImpl<Self>>::impl_len(self)
+        }
 
-impl DimsList for Nil {}
+        fn to_vec(&self) -> Vec<usize>
+        where
+            (): impls::ToVecImpl<Self>,
+        {
+            <() as impls::ToVecImpl<_>>::impl_to_vec(self)
+        }
 
-pub trait StaticDimsList
-where
-    Self: DimsList,
-{
-}
-impl<Head, Tail> StaticDimsList for Cons<Head, Tail>
-where
-    Head: Unsigned + Dim,
-    Tail: List + StaticDimsList,
-{
-}
-
-impl StaticDimsList for Nil {}
-
-/// Marks a single dimension.
-pub trait Dim {
-    fn to_usize(&self) -> usize;
-    fn to_dyn(&self) -> DynDim;
-}
-
-impl Dim for DynDim {
-    fn to_usize(&self) -> usize {
-        self.0
-    }
-
-    fn to_dyn(&self) -> DynDim {
-        self.clone()
-    }
-}
-impl Dim for UTerm {
-    fn to_usize(&self) -> usize {
-        Self::USIZE
-    }
-
-    fn to_dyn(&self) -> DynDim {
-        DynDim::new(Self::USIZE)
-    }
-}
-impl<U, B> Dim for UInt<U, B>
-where
-    U: Unsigned,
-    B: Bit,
-{
-    fn to_usize(&self) -> usize {
-        Self::USIZE
+        fn matrix_dot<Rhs>(
+            &self,
+            rhs: &Rhs,
+        ) -> impls::MatrixDotImplOp<Self, Rhs, ops::MatrixDotOp<Self, Rhs>>
+        where
+            Self: Sized,
+            Rhs: Dimensions,
+            (): ops::MatrixDot<Self, Rhs>
+                + impls::MatrixDotImpl<Self, Rhs, ops::MatrixDotOp<Self, Rhs>>,
+        {
+            <() as impls::MatrixDotImpl<Self, Rhs, ops::MatrixDotOp<Self, Rhs>>>::impl_matrix_dot(
+                self, rhs,
+            )
+        }
     }
 
-    fn to_dyn(&self) -> DynDim {
-        DynDim::new(Self::USIZE)
+    impl Dimensions for DynDimensions {}
+
+    impl<Head, Tail> Dimensions for Cons<Head, Tail>
+    where
+        Head: Dim,
+        Tail: DimsList,
+    {
     }
+
+    impl Dimensions for Nil {}
+
+    pub trait DimsList
+    where
+        Self: List + Dimensions,
+    {
+    }
+
+    impl<Head, Tail> DimsList for Cons<Head, Tail>
+    where
+        Head: Dim,
+        Tail: DimsList,
+    {
+    }
+
+    impl DimsList for Nil {}
+
+    pub trait StaticDimsList
+    where
+        Self: DimsList,
+    {
+    }
+    impl<Head, Tail> StaticDimsList for Cons<Head, Tail>
+    where
+        Head: Unsigned + Dim,
+        Tail: List + StaticDimsList,
+    {
+    }
+
+    impl StaticDimsList for Nil {}
 }
 
-pub type DynDim = Dyn<usize>;
+mod dim {
+    use super::*;
 
-/// The dimensions with runtime length.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct DynDimensions(pub Vec<usize>);
+    // alias
 
-// aliases
+    pub type DynDim = Dyn<usize>;
 
-pub type Dims1<P> = DimsT![P];
-pub type Dims2<P, Q> = DimsT![P, Q];
-pub type Dims3<P, Q, R> = DimsT![P, Q, R];
+    // trait
+
+    /// Marks a single dimension.
+    pub trait Dim {
+        fn to_usize(&self) -> usize;
+        fn to_dyn(&self) -> DynDim;
+    }
+
+    impl Dim for DynDim {
+        fn to_usize(&self) -> usize {
+            self.0
+        }
+
+        fn to_dyn(&self) -> DynDim {
+            self.clone()
+        }
+    }
+    impl Dim for UTerm {
+        fn to_usize(&self) -> usize {
+            Self::USIZE
+        }
+
+        fn to_dyn(&self) -> DynDim {
+            DynDim::new(Self::USIZE)
+        }
+    }
+    impl<U, B> Dim for UInt<U, B>
+    where
+        U: Unsigned,
+        B: Bit,
+    {
+        fn to_usize(&self) -> usize {
+            Self::USIZE
+        }
+
+        fn to_dyn(&self) -> DynDim {
+            DynDim::new(Self::USIZE)
+        }
+    }
+}
 
 // dynamic dim
 
